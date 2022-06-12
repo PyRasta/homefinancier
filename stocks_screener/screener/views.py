4639d20ed715a6cef054d2dbf60616cd84705110
PyRasta <daniilrastorguev01@gmail.com>
2022-06-12 13:22:53 +0500

    update_views

--- stocks_screener/screener/views.py
+++ stocks_screener/screener/views.py
@@ -26,7 +26,7 @@ def ema10_up(dict_ema10):
         return True
 
 
-def ema10_dowm(dict_ema10):
+def ema10_down(dict_ema10):
      if dict_ema10['value_open'] < dict_ema10['ema10'] and dict_ema10['value_close'] < dict_ema10['ema10']:
         return True
 
@@ -35,7 +35,7 @@ def intersection_macd(handler):
     analysis = handler.get_analysis()
     macd_macd = analysis.indicators['MACD.macd']
     macd_signal = analysis.indicators['MACD.signal']
-    if round(float(macd_macd), 4) == round(float(macd_signal), 4):
+    if round(float(macd_macd), 3) == round(float(macd_signal), 3):
         return True
 
 def rsi_pattern_up(handler):
@@ -51,37 +51,56 @@ def rsi_pattern_down(handler):
         return True
 
 
-def index(request):
-    list_stocks = Tv.objects.using('list_stocks').filter(screener='america').filter(exchange='NASDAQ')
-
+def find_pattern_stocks(stock):
     interval = Interval.INTERVAL_1_DAY
+    stock_patterns = {}
+    handler = TA_Handler(
+        symbol=stock.symbol,
+        exchange=stock.exchange,
+        screener='america',
+        interval=interval
+    try:
+        dict_ema10 = get_ema10(handler)
+        if intersection_up_ema10(dict_ema10):
+            stock_patterns['symbol'] = stock.symbol
+            stock_patterns['intersection_ema10_up'] = True
+        else:
+            stock_patterns['symbol'] = stock.symbol
+            stock_patterns['intersection_ema10_up'] = False
+
+        if intersection_down_ema10(dict_ema10):
+            stock_patterns['intersection_ema10_down'] = True
+        else:
+            stock_patterns['intersection_ema10_down'] = False
+
+        if ema10_up(dict_ema10):
+            stock_patterns['ema10_up'] = True
+        else:
+            stock_patterns['ema10_up'] = False
+
+        if ema10_down(dict_ema10):
+            stock_patterns['ema10_down'] = True
+        else:
+            stock_patterns['ema10_down'] = False
+            
+        if intersection_macd(handler):
+            stock_patterns['macd'] = True
+        else:
+            stock_patterns['macd'] = False
+
+        if rsi_pattern_up(handler):
+            stock_patterns['rsi_up'] = True
+        else:
+            stock_patterns['rsi_up'] = False
+            
+        if rsi_pattern_down(handler):
+            stock_patterns['rsi_down'] = True
+        else:
+            stock_patterns['rsi_down'] = False
+        return stock_patterns        
+    except:
+        return False
+
+        
+def views_stocks(request):
     
-    for stock in list_stocks:
-        handler = TA_Handler(
-            symbol=stock.symbol,
-            exchange=stock.exchange,
-            screener='america',
-            interval=interval
-        )
-        try:
-            dict_ema10 = get_ema10(handler)
-            if rsi_pattern_up(handler) and intersection_macd(handler) and intersection_up_ema10(dict_ema10):
-                print(f'RSI Патерн Вверх: {stock.symbol}')
-            if rsi_pattern_down(handler) == True:
-                print(f'RSI Патерн Вниз: {stock.symbol}')
-        except:
-            print('Потрачено')
-        #try:
-            #if intersection_macd(handler) == True:
-               # print(f'Пересечение macd: {stock.symbol}')
-        #except:
-            #print('Потрачено')
-        #try:
-            #dict_ema10 = get_ema10(handler)
-            #if intersection_up_ema10(dict_ema10) == True:
-                #print(f'Пересечение вверх {stock.symbol}')
-        #except:
-           #print('Не получено')
-
-    return render(request, 'screener/index.html')
-
